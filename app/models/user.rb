d037169b3f290be87e56b567918ab5342e734f84
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-05-26 16:43:57 +0100

    Associate comments with posts and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
 
   validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 12 }
   validates :password, presence: true, length: { minimum: 6 }
