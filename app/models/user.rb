1bb8a18dd77dc9fd6e8b2f4e1970576eb54ff25e
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-05-26 16:17:02 +0100

    Migrate post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :posts
-  
-  validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 12}
+
+  validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 12 }
   validates :password, presence: true, length: { minimum: 6 }
   validates :email, presence: true
 end
