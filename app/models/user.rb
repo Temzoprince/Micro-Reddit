eb4666c141adf60edd98f04ffc1899089937608c
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-05-26 16:06:33 +0100

    Add validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 12}
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :email, presence: true
 end
